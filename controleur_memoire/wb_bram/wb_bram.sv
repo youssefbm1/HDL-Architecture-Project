ee39ca445b66b8721627f8f3b828e4d12c5e9279
Youssef Ben Mbarek <benmbarek-23@tp-3a101-19.enst.fr>
2024-01-21 22:51:07 +0100

    Pipelined

--- controleur_memoire/wb_bram/wb_bram.sv
+++ controleur_memoire/wb_bram/wb_bram.sv
@@ -13,9 +13,11 @@ module wb_bram #(
 );
 
   // Déclaration des signaux logiques
-  logic ack_write, ack_read, ack_read_assync;
+  logic ack_write, ack_read, ack_read_assync, adress_counter = 0;
   logic [7:0][mem_adr_width-1:0] memory[0:(1<<mem_adr_width)-1];
-  wire [mem_adr_width-1:0] address_input = wb_s.adr[mem_adr_width+1:2];
+  wire [mem_adr_width-1:0] address_input;
+
+  assign address_input = wb_s.adr[mem_adr_width+1:2] + adress_counter;
 
   // Initialisation des signaux d'erreur et de rety
   assign wb_s.err = 0;
@@ -29,37 +31,35 @@ module wb_bram #(
   assign wb_s.ack = ack_write | ack_read;
 
   // Bloc always_ff pour la synchronisation des signaux
-  always_ff @(posedge wb_s.clk) begin
+always_ff @(posedge wb_s.clk) begin
     // Logique de réinitialisation
     if (wb_s.rst || ack_read_assync) begin
-      ack_read <= !wb_s.rst;
+        ack_read <= !wb_s.rst;
     end
 
     // Logique de lecture
     if (ack_read_assync) begin
-      // Cycle de bus classique
-      if (!(wb_s.cti[0] || wb_s.cti[1])) begin // cti = '000'
-        if (!ack_read) begin
-          for (int i = 0; i < 4; i++) begin
-            if (wb_s.sel[i]) begin
-              wb_s.dat_sm[(8*i+7)-:8] <= memory[address_input][i];
-            end
-          end
+        // Cycle de bus classique
+        if (!(wb_s.cti[2] || wb_s.cti[1])) begin // cti = '000'
+          adress_counter <= 0;
+          ack_read <= !ack_read;
         end
-        ack_read <= !ack_read;
+      else if (wb_s.cti[1]) begin 
+        adress_counter <= 1;
+      end else begin 
+        adress_counter <= 0;
       end
+      wb_s.dat_sm <= memory[address_input];
     end
 
-        // Logique d'écriture
+    // Logique d'écriture
     if (ack_write) begin
-        // Cycle de bus classique pour l'écriture
-        if (!(wb_s.cti[0] || wb_s.cti[1])) begin // cti = '000'
-            for (int i = 0; i < 4; i++) begin
-                if (wb_s.sel[i]) begin
-                    memory[address_input][i] <= wb_s.dat_ms[(8*i+7)-:8];
-                end
-            end
+      for (int i = 0; i < 4; i++) begin
+        if (wb_s.sel[i]) begin
+          memory[address_input][i] <= wb_s.dat_ms[(8*i+7)-:8];
         end
+      end
     end
   end
+
 endmodule
